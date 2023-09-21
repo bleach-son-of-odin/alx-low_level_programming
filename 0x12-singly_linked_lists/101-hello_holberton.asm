cdbef17d400d5677c3a9a0476df5747df2327c77
bleach-son-of-odin <jasontoddmich@gmail.com>
2023-09-21 20:04:50 +0000

    Write a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.
    You are only allowed to use the printf function
    You are not allowed to use interrupts

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,10 @@
-section .data
-hello db "Hello, Holberton",0
-format db "%s",0
-
-section .text
-global main
-extern printf
-
-main:
-push rbp
-mov rdi, format
-mov rsi, hello
-call printf
-pop rbp
-
-; Exit the program
-mov rax, 60         ; syscall number for exit (60)
-xor rdi, rdi        ; status: 0
-syscall
+   global    main
+   extern    printf
+   main:
+   mov   edi, format
+   xor   eax, eax
+   call  printf
+   mov 	eax, 0
+   ret
+   format: db `Hello, Holberton\n`,0
 
